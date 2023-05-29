c326914b8afa8dee6a1c1014df406d1e94924bc0
Jacek <yaca5@o2.pl>
2023-05-29 23:26:00 +0200

    Zadanie domowe dzień 4

--- ChallenegeApp/ChallenegeApp/Program.cs
+++ ChallenegeApp/ChallenegeApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Maria";
+bool gender = false;
+int age = 28;
+if (age < 30 && gender == false)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (age == 30 && name == "Ewa" && gender == false)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (age < 18 && gender == true)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Żaden warunek nie jest spelniony");
+}
