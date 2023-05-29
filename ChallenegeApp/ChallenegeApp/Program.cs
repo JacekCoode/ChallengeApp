ba03542bc5a795858543c6fe4de404199dad1301
Jacek <yaca5@o2.pl>
2023-05-29 23:31:23 +0200

    Zadanie domowe dzień 5

--- ChallenegeApp/ChallenegeApp/Program.cs
+++ ChallenegeApp/ChallenegeApp/Program.cs
@@ -1,19 +1,55 @@
-﻿string name = "Maria";
-bool gender = false;
-int age = 28;
-if (age < 30 && gender == false)
+﻿int number = 4566;
+string numberInString = number.ToString();
+char[] letters = numberInString.ToCharArray();
+string[] decNumber = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+int[] countTab = new int[10];
+
+foreach (char leter in letters)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+    if (leter == '0')
+    {
+        countTab[0]++;
+    }
+    else if (leter == '1')
+    {
+        countTab[1]++;
+    }
+    else if (leter == '2')
+    {
+        countTab[2]++;
+    }
+    else if (leter == '3')
+    {
+        countTab[3]++;
+    }
+    else if (leter == '4')
+    {
+        countTab[4]++;
+    }
+    else if (leter == '5')
+    {
+        countTab[5]++;
+    }
+    else if (leter == '6')
+    {
+        countTab[6]++;
+    }
+    else if (leter == '7')
+    {
+        countTab[7]++;
+    }
+    else if (leter == '8')
+    {
+        countTab[8]++;
+    }
+    else if (leter == '9')
+    {
+        countTab[9]++;
+    }
 }
-else if (age == 30 && name == "Ewa" && gender == false)
+Console.WriteLine("Wynik dla liczby: " + number);
+for (int i = 0; i < 10; i++)
 {
-    Console.WriteLine("Ewa, lat 33");
-}
-else if (age < 18 && gender == true)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
-else
-{
-    Console.WriteLine("Żaden warunek nie jest spelniony");
+    string display = decNumber[i] + " => " + countTab[i];
+    Console.WriteLine(display);
 }
