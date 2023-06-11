c38068284aebccc92b6175eb7cebcce60131c935
Jacek <yaca5@o2.pl>
2023-06-11 13:48:32 +0200

    Zadanie domowe dzień 6

--- ChallenegeApp/ChallenegeApp/Program.cs
+++ ChallenegeApp/ChallenegeApp/Program.cs
@@ -1,55 +1,45 @@
-﻿int number = 4566;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToCharArray();
-string[] decNumber = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-int[] countTab = new int[10];
+﻿using ChallenegeApp;
+Employee employee1 = new Employee("Adam", "Kowalski", 34);
+Employee employee2 = new Employee("Monika", "Nowak", 25);
+Employee employee3 = new Employee("Zuzia", "Wisniewska", 30);
 
-foreach (char leter in letters)
+employee1.AddScore(6);
+employee1.AddScore(2);
+employee1.AddScore(7);
+employee1.AddScore(1);
+employee1.AddScore(2);
+
+employee2.AddScore(3);
+employee2.AddScore(3);
+employee2.AddScore(1);
+employee2.AddScore(4);
+employee2.AddScore(9);
+
+employee3.AddScore(5);
+employee3.AddScore(3);
+employee3.AddScore(2);
+employee3.AddScore(1);
+employee3.AddScore(1);
+
+List<Employee> employees = new List<Employee>()
 {
-    if (leter == '0')
-    {
-        countTab[0]++;
-    }
-    else if (leter == '1')
-    {
-        countTab[1]++;
-    }
-    else if (leter == '2')
-    {
-        countTab[2]++;
-    }
-    else if (leter == '3')
-    {
-        countTab[3]++;
-    }
-    else if (leter == '4')
-    {
-        countTab[4]++;
-    }
-    else if (leter == '5')
-    {
-        countTab[5]++;
-    }
-    else if (leter == '6')
-    {
-        countTab[6]++;
-    }
-    else if (leter == '7')
-    {
-        countTab[7]++;
-    }
-    else if (leter == '8')
-    {
-        countTab[8]++;
-    }
-    else if (leter == '9')
-    {
-        countTab[9]++;
-    }
-}
-Console.WriteLine("Wynik dla liczby: " + number);
-for (int i = 0; i < 10; i++)
+    employee1, employee2, employee3
+};
+
+int maxResult = 0;
+Employee userWithMaxResult = null;
+
+foreach (var employee in employees)
 {
-    string display = decNumber[i] + " => " + countTab[i];
-    Console.WriteLine(display);
+    if (employee.Result > maxResult)
+    {
+        userWithMaxResult = employee;
+        maxResult = employee.Result;
+    }  
 }
+
+Console.WriteLine("Dane pracownika z najwyższą liczbą punktów: ");
+Console.WriteLine(userWithMaxResult.Name + " " + userWithMaxResult.LastName);
+Console.WriteLine("Wiek: " + userWithMaxResult.Age);
+Console.WriteLine("Punkty: " + userWithMaxResult.Result);
+
